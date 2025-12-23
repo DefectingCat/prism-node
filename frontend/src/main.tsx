8dc1762a4930f4f3f239b3689ad8f4e544ad4dcf
xfy <i@rua.plus>
2025-12-23 14:15:06 +0800

    feat: add home page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router';
+import { BrowserRouter } from 'react-router';
 import App from './App.tsx';
 import './index.css';
 
@@ -10,9 +10,7 @@ if (!rootElement) throw new Error('Root element not found');
 createRoot(rootElement).render(
   <StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </StrictMode>,
 );
